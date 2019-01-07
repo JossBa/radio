77665d25dd9b364b7c5d38f81203717f54cf287f
Nadav <nadavbabai@hotmail.com>
2019-01-07 19:43:22 +0100

    structure migration add drop if exists.

--- messenger-model/src/META-INF/scripts/sql/radio-mariadb-structure-migration.sql
+++ messenger-model/src/META-INF/scripts/sql/radio-mariadb-structure-migration.sql
@@ -1,5 +1,9 @@
 SET CHARACTER SET utf8;
 USE radio;
 
+
+ALTER TABLE Person DROP IF  EXISTS lastTransmissionTimestamp;
+ALTER TABLE Person DROP IF  EXISTS lastTransmissionAddress;
+
 ALTER TABLE Person ADD IF NOT EXISTS lastTransmissionTimestamp BIGINT NULL;
 ALTER TABLE Person ADD IF NOT EXISTS lastTransmissionAddress VARCHAR(63) NULL;
